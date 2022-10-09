c3eb1f38dd3391a604a887c528d5d771e16ef3d0
MUSAB MUBARAQ MBURAIMOH <114432105+kingmusab-tmt@users.noreply.github.com>
2022-10-09 22:25:36 +0000

    First Version

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const myInput = ["3229 + 698", "3 - 5900", "500 + 2009", "f + 4993", "3 - 2090"];
+const myInput = ["3232 - 698", "3 - 5900", "500 + 209", "2322 + 4993", "3 - 2090"];
+function arithmetic_arranger(myInput){
 if(myInput.length > 5){
-  console.log("Too Many Problems")
+  console.log ("Too Many Problems")
   }  else {
     for (value of myInput) {
       let item = value.split(" ")
-    console.log(item)
+      let numm1 = item[0];
+      let numm2 = item[2];
+      numm1 = +numm1;
+      numm2 = +numm2;
+      if (isNaN(numm1) || isNaN(numm2)){
+        console.log ("Numbers must only contain digits");
+      } else if (item[1] !== "+" && item[1] !== "-"){
+        console.log("Operator must be '+' or '-' ")
+      } else {
       if (item[0].length > 4 || item[2].length > 4){
       console.log("Numbers cannot be more than four digits \n")
     } else{
 if (value.length <= 11 && value.includes("+")) {
     value = value.split("+")
+    
     if (value[0].length > value[1].length) {
       if((value[0].length - value[1].length) == 3){
        console.log(`  ${value[0]}`)
@@ -141,6 +151,7 @@ if (value.length <= 11 && value.includes("+")) {
       console.log(`   ${diff}\n`);
     }
      } 
-        }
+        }}
     }
-}
+}}
+arithmetic_arranger(myInput);
